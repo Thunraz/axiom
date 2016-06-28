5dce58fd44268e33b8d966d1e940c241a91c85ed
Julian <julian.dinges@men.de>
2016-06-28 13:41:43 +0200

    Using planet in 3.js

--- js/app/3.js
+++ js/app/3.js
@@ -1,37 +1,42 @@
-define(['app/config', 'three', 'app/controls', 'app/inputHandler'],function(config, THREE, controls, inputHandler) {
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(config.camera.fov, config.canvasWidth/config.canvasHeight, 0.1, 1000);
+'use strict';
+define(
+    [
+        'app/config',
+        'three',
+        'app/controls',
+        'app/inputHandler',
+        'app/classes/Planet'
+    ],
+    function(config, THREE, controls, inputHandler, Planet) {
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera(config.camera.fov, config.canvasWidth/config.canvasHeight, 0.1, 1000);
 
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(config.canvasWidth, config.canvasHeight);
-    document.getElementById('game').appendChild( renderer.domElement );
+        var renderer = new THREE.WebGLRenderer();
+        renderer.setSize(config.canvasWidth, config.canvasHeight);
+        document.getElementById('game').appendChild( renderer.domElement );
 
-    var geometry = new THREE.SphereGeometry(1.5, 32, 32);
-    var material = new THREE.MeshPhongMaterial({color: 0x33ff33});
-    var cube = new THREE.Mesh(geometry, material);
+        var ambientLight = new THREE.AmbientLight( 0x101010 ); // soft white light
+        scene.add( ambientLight );
 
-    scene.add(cube);
+        var pointLight = new THREE.PointLight( 0x111111, 4 * Math.PI, 100 );
+        pointLight.position.set( 1, 1, 2 );
+        scene.add( pointLight );
 
-    var ambientLight = new THREE.AmbientLight( 0x101010 ); // soft white light
-    scene.add( ambientLight );
+        camera.position.z = 50;
+        
+        var homePlanet = new Planet('home', 1234.0, 100.0, new THREE.Vector3(0.0, 0.0, 0.0), true, 0x33ff33);
+        scene.add(homePlanet.mesh);
 
-    var pointLight = new THREE.PointLight( 0x111111, 4 * Math.PI, 100 );
-    pointLight.position.set( 1, 1, 2 );
-    scene.add( pointLight );
+        function update() {
+            requestAnimationFrame(update);
 
-    camera.position.z = 50;
+            inputHandler.checkInput();
 
-    function update() {
-        requestAnimationFrame(update);
+            renderer.render(scene, camera);
 
-        inputHandler.checkInput();
+            inputHandler.checkInput(camera);
+        }
 
-        cube.rotation.x += 0.1;
-        cube.rotation.y += 0.1;
-        renderer.render(scene, camera);
-
-        inputHandler.checkInput(camera);
+        update();
     }
-
-    update();
-});
+);
