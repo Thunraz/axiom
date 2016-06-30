86761ccab092d841703e7a1723ba88dc378bcf84
Thunraz <julian.dinges@gmail.com>
2016-06-30 23:22:04 +0200

    Added acceleration vector

--- js/app/classes/spaceObject.js
+++ js/app/classes/spaceObject.js
@@ -5,8 +5,9 @@ define(
         return class SpaceObject {
             constructor() {
                 this.mass = 1.0;
-                this.position = new THREE.Vector3(0.0, 0.0, 0.0);
-                this.velocity = new THREE.Vector3(0.0, 0.0, 0.0);
+                this.position     = new THREE.Vector3(0.0, 0.0, 0.0);
+                this.velocity     = new THREE.Vector3(0.0, 0.0, 0.0);
+                this.acceleration = new THREE.Vector3(0.0, 0.0, 0.0);
             }
 
             updatePosition(deltaT) { }
