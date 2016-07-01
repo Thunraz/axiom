9fd942b5f1f21e58aabd4456f9e255f0003f8346
Julian <julian.dinges@men.de>
2016-07-01 14:00:14 +0200

    Added TrailParticle

---
+++ js/app/classes/TrailParticle.js
@@ -0,0 +1,50 @@
+'use strict';
+define(
+    ['three', 'app/classes/spaceObject'],
+    function(THREE, SpaceObject) {
+        return class TrailParticle extends SpaceObject {
+            constructor(scene, parent, position, size, lifetime) {
+                super();
+
+                this.scene    = scene;
+
+                this.size     = size;
+                this.position = position;
+                this.color    = parent.color;
+
+                this.lifetime = lifetime;
+                this.life     = 0.0;
+                this.alive    = this.life < this.lifetime;
+                
+                this.mesh     = null;
+                this._createMesh();
+                this.scene.add(this.mesh);
+            }
+
+            _createMesh() {
+                let geometry = new THREE.BoxGeometry(this.size, this.size, this.size);
+                let material = new THREE.MeshBasicMaterial({color: this.color});
+                
+                this.mesh = new THREE.Mesh(geometry, material);
+                this.mesh.position.set(this.position.x, this.position.y, this.position.z);
+            }
+
+            update(deltaT) {
+                if(this.life <= this.lifetime) {
+                    let factor = 1 - this.life / this.lifetime / 50;
+
+                    // Fade to black (alpha not available)
+                    this.mesh.material.color.r *= factor;
+                    this.mesh.material.color.g *= factor;
+                    this.mesh.material.color.b *= factor;
+
+                    this.life += deltaT / 1000;
+                } else {
+                    this.alive = false;
+                    this.scene.remove(this.mesh);
+                }
+            }
+
+        }
+    }
+);
