2e02abbda4c4ad670841a34088d39d19ed533698
Julian <julian.dinges@men.de>
2016-07-01 13:59:42 +0200

    Moved some stuff from SpaceObject to AstronomicalObject

--- js/app/classes/AstronomicalObject.js
+++ js/app/classes/AstronomicalObject.js
@@ -5,6 +5,46 @@ define(
         return class AstronomicalObject extends SpaceObject {
             constructor() {
                 super();
+                
+                this.mass         = 0.0;
+
+                this.velocity     = new THREE.Vector3(0.0, 0.01, 0.0);
+                this.acceleration = new THREE.Vector3(0.0, 0.0, 0.0);
+
+                this.counter = 0;
+            }
+
+            updatePosition(time, deltaT, spaceObjects) {
+                /*
+                Velocity Verlet:
+
+                acceleration = force(time, position) / mass;
+                time += timestep;
+                position += timestep * (velocity + timestep * acceleration / 2);
+                newAcceleration = force(time, position) / mass;
+                velocity += timestep * (acceleration + newAcceleration) / 2;
+
+                */
+                this.acceleration = this.force(time, this.position, spaceObjects).divideScalar(this.mass);
+
+                this.position = this.position.add(
+                    this.velocity.add(this.acceleration.divideScalar(2))
+                );
+
+                let newAcceleration = this.force(time, this.position, spaceObjects).divideScalar(this.mass);
+                this.velocity = this.velocity.add(
+                    this.acceleration.add(newAcceleration).multiplyScalar(deltaT / 2)
+                );
+
+                /*if (this.counter <= 10) {
+                    //console.log(this.name);
+                    console.log(this.velocity);
+                    this.counter++;
+                }*/
+            }
+
+            force(time, position) {
+                return new THREE.Vector3(-position.x / time, -position.y / time, 0.0);
             }
         }
     }
--- js/app/classes/spaceObject.js
+++ js/app/classes/spaceObject.js
@@ -4,39 +4,10 @@ define(
     function(THREE) {
         return class SpaceObject {
             constructor() {
-                this.mass = 1.0;
-                this.position     = new THREE.Vector3(0.0, 0.0, 0.0);
-                this.velocity     = new THREE.Vector3(1.0, 0.0, 0.0);
-                this.acceleration = new THREE.Vector3(0.0, 0.0, 0.0);
-
-                this.counter = 0;
-            }
-
-            updatePosition(time, deltaT) {
-                /*
-                Velocity Verlet:
-
-                acceleration = force(time, position) / mass;
-                time += timestep;
-                position += timestep * (velocity + timestep * acceleration / 2);
-                */
-                this.acceleration = this.force(time, this.position).divideScalar(this.mass);
-                this.position = this.position.add(
-                    this.velocity.add(this.acceleration.multiplyScalar(deltaT / 2)).multiplyScalar(deltaT)
-                );
-
-                if (this.counter <= 10) {
-                    console.log(this.name);
-                    console.log(this.velocity);
-                    this.counter++;
-                }
+                this.position = new THREE.Vector3(0.0, 0.0, 0.0);
             }
 
             update(deltaT) { }
-
-            force(time, position) {
-                return new THREE.Vector3(1.0, 1.0, 1.0);
-            }
         }
     }
 );
