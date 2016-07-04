55fac391a14e37fb39697b1982bad03c09628172
Thunraz <thunraz@thunraz.com>
2016-07-04 10:06:53 +0200

    Some more beatifying

--- js/app/classes/spaceObject.js
+++ js/app/classes/spaceObject.js
@@ -3,6 +3,26 @@ define(
     ['three'],
     function(THREE) {
         return class SpaceObject {
+
+            // ##############################################
+            // # Constructor ################################
+            // ##############################################
+
+            constructor() {
+                this.id       = SpaceObject.NEXT_ID;
+                this.position = new THREE.Vector3(0.0, 0.0, 0.0);
+            }
+
+            // ##############################################
+            // # Public functions ###########################
+            // ##############################################
+
+            update(deltaT) { }
+
+            // ##############################################
+            // # Static functions ###########################
+            // ##############################################
+            
             static get NEXT_ID() {
                 if(this._NEXT_ID == undefined) {
                     this._NEXT_ID = 0;
@@ -14,12 +34,8 @@ define(
                 return val;
             }
 
-            constructor() {
-                this.id       = SpaceObject.NEXT_ID;
-                this.position = new THREE.Vector3(0.0, 0.0, 0.0);
-            }
+            // ##############################################
 
-            update(deltaT) { }
-        }
+        } // class
     }
 );
