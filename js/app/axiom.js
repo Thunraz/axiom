3ce2cad6efcc473a0dee932080807811a4dd4629
Thunraz <thunraz@thunraz.com>
2016-07-04 09:14:08 +0200

    Tweaked settings a bit

--- js/app/axiom.js
+++ js/app/axiom.js
@@ -23,7 +23,7 @@ define(
 
         let astroObjects = [
             new Planet(scene, 'home',  1234.0, 100.0, new THREE.Vector3(-10.0, 5.0, 0.0), true, 0x33ff33),
-            new Planet(scene, 'second', 234.0,  50.0, new THREE.Vector3(7.0, 3.0, 0.0),   true, 0xff3333),
+            new Planet(scene, 'second', 80034.0,  50.0, new THREE.Vector3(7.0, 3.0, 0.0),   true, 0xff3333),
 
             new Star(scene, 'sol', 400000.0, 300.0, new THREE.Vector3(0.0, 0.0, 0.0), 0xffff00)
         ];
@@ -31,8 +31,8 @@ define(
         astroObjects[0].velocity.setX(.01);
         astroObjects[0].velocity.setY(.02);
 
-        astroObjects[1].velocity.setX(.02);
-        astroObjects[1].velocity.setY(-.02);
+        astroObjects[1].velocity.setX(.01);
+        astroObjects[1].velocity.setY(.05);
 
         let lastFrameTime = 0;
 
