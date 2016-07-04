92db849b48f87746ca69d4edfa222297a62d6a93
Julian <julian.dinges@men.de>
2016-07-04 14:29:49 +0200

    It's now possible to view direction vectors

--- js/app/classes/AstronomicalObject.js
+++ js/app/classes/AstronomicalObject.js
@@ -1,7 +1,7 @@
 'use strict';
 define(
-    ['app/classes/SpaceObject', 'app/classes/Constants'],
-    function(SpaceObject, Constants) {
+    ['app/classes/SpaceObject', 'app/classes/Constants', 'app/config'],
+    function(SpaceObject, Constants, config) {
         return class AstronomicalObject extends SpaceObject {
 
             // ##############################################
@@ -15,6 +15,34 @@ define(
 
                 this.velocity     = new THREE.Vector3(0.0, 0.0, 0.0);
                 this.acceleration = new THREE.Vector3(0.0, 0.0, 0.0);
+
+                if(config.showDirectionalVectors) {
+                    this.arrowGeometry = new THREE.BufferGeometry();
+                }
+            }
+
+            // ##############################################
+            // # Private functions ##########################
+            // ##############################################
+
+            _createDirectionalArrow() {
+                let vertices = new Float32Array([
+                    this.position.x, this.position.y, this.position.z,
+
+                    5.0, 5.0, 10.0
+                ]);
+
+                let colors = new Float32Array([
+                    ((this.color & 0xff0000) >> 16) / 256, ((this.color & 0x00ff00) >> 8) / 256, (this.color & 0x0000ff) / 256,
+                    ((this.color & 0xff0000) >> 16) / 256, ((this.color & 0x00ff00) >> 8) / 256, (this.color & 0x0000ff) / 256,
+                ]);
+
+                this.arrowGeometry.addAttribute('position', new THREE.BufferAttribute(vertices, 3));
+                this.arrowGeometry.addAttribute('color', new THREE.BufferAttribute(colors, 3));
+
+                let material = new THREE.LineBasicMaterial({ vertexColors: THREE.VertexColors });
+
+                return new THREE.Line(this.arrowGeometry, material);
             }
 
             // ##############################################
@@ -28,6 +56,19 @@ define(
                     this.velocity
                         .add(this.acceleration.multiplyScalar(deltaT / 2))
                 );
+
+                // Only calculate this if config value is set
+                if(config.showDirectionalVectors) {
+                    let directionalVector = this.position.clone();
+                    directionalVector.add(this.velocity.clone().normalize().multiplyScalar(this.velocity.length() * this.radius));
+                    
+                    let vertices = new Float32Array([
+                        this.position.x, this.position.y, this.position.z,
+
+                        directionalVector.x, directionalVector.y, directionalVector.z
+                    ]);
+                    this.arrowGeometry.addAttribute('position', new THREE.BufferAttribute(vertices, 3));
+                }
             }
 
             // ##############################################
