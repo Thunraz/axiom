a174740f73552285c4f52cba558ad76afc984711
Thunraz <julian.dinges@gmail.com>
2016-07-06 15:10:38 +0200

    Loading SpaceShip mesh and position indicator

--- js/app/classes/SpaceShip.js
+++ js/app/classes/SpaceShip.js
@@ -11,20 +11,54 @@ define(
             constructor(scene, name, mass, position) {
                 super();
 
-                this.scene = scene;
-                this.name  = name;
+                this.scene    = scene;
 
-                let loader = new THREE.ObjectLoader();
-                loader.load('assets/models/ship.json', function(geometry, materials) {
-                    let material = new THREE.MultiMaterial(materials);
-                    let object   = new THREE.Mesh(geometry, material);
-                    scene.add(object);
+                this.name     = name;
+                this.mass     = mass;
+                this.position = position;
+
+                let that   = this;
+
+                let loader = new THREE.JSONLoader();
+                loader.load('assets/models/ship.json', function(shipGeometry, shipMaterials) {
+                    let shipMaterial = new THREE.MultiMaterial(shipMaterials);
+                    that.shipMesh    = new THREE.Mesh(shipGeometry, shipMaterial);
+                    scene.add(that.shipMesh);
+
+                    // Create position indicator
+                    let positionIndicatorGeometry = new THREE.Geometry();
+                    
+                    let numberSegments = 16;
+                    let circleSegment = 2 * Math.PI / numberSegments;
+                    let circleRadius = that.shipMesh.geometry.boundingSphere.radius;
+
+                    for(let i = 0; i <= numberSegments; i++) {
+                        let vector = new THREE.Vector3(
+                            circleRadius * Math.cos(i * circleSegment),
+                            circleRadius * Math.sin(i * circleSegment),
+                            0
+                        );
+
+                        positionIndicatorGeometry.vertices.push(vector);
+                    }
+                    
+                    let positionIndicatorMaterial = new THREE.LineBasicMaterial({ color: 0x7fffd4 });
+                    that.positionIndicator = new THREE.Line(positionIndicatorGeometry, positionIndicatorMaterial);
+                    scene.add(that.positionIndicator);
                 });
             }
 
             // ##############################################
             // # Public functions ###########################
             // ##############################################
+            
+            update(deltaT, spaceObjects) {
+                super.update(deltaT, spaceObjects);
+
+                if(this.shipMesh == null) return;
+                this.shipMesh.position.set(this.position.x, this.position.y, this.position.z);
+                this.positionIndicator.position.set(this.position.x, this.position.y, this.position.z);
+            }
         }
     }
 );
