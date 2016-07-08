73dd341b44f2fc65efa62d082270fa8a9c793fc3
Thunraz <julian.dinges@gmail.com>
2016-07-08 13:59:25 +0200

    Some WIP stuff

--- js/app/axiom.js
+++ js/app/axiom.js
@@ -24,13 +24,17 @@ define(
         renderer.setSize(config.canvasWidth, config.canvasHeight);
         document.getElementById('game').appendChild( renderer.domElement );
         
+        let cameraPivot = new THREE.Object3D();
+
         let camera = new THREE.PerspectiveCamera(config.camera.fov, config.canvasWidth / config.canvasHeight, 0.1, 1000);
         camera.rotation.x = 0.75;
         camera.position.y = -465;
         camera.position.z = 500;
         camera.zoom       = 5;
         camera.updateProjectionMatrix();
-        scene.camera = camera;
+        cameraPivot.add(camera);
+        scene.add(cameraPivot)
+        scene.cameraPivot = cameraPivot;
 
         let ambientLight = new THREE.AmbientLight( 0x101010 ); // soft white light
         scene.add( ambientLight );
@@ -43,7 +47,7 @@ define(
             new Grid(scene, 'grid', new THREE.Vector3(0, 0, 0), 75, 75, 6, true),
 
             new Planet(scene, 'firstPlanet',    3.301,    21.036, new THREE.Vector3(18, 13, 0), true, 0xff3333),
-            new Planet(scene, 'homePlanet',    48.690, 100.48794, new THREE.Vector3(20, 5, 0), true, 0x33ff33),
+            new Planet(scene, 'homePlanet',    48.690, 100.48794, new THREE.Vector3(20,  5, 0), true, 0x33ff33),
             
             new Star(scene, 'sol', 1.9984E7, 1392.684, new THREE.Vector3(0, 0, 0), 0xffff00),
 
@@ -80,8 +84,6 @@ define(
 
             // Update all the objects
             GameObjectManager.update(deltaT);
-
-            Debug.log(GameObjectManager.get('firstPlanet').position);
             
             // Render the scene
             renderer.render(scene, camera);
