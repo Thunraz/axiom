28899fc47be2cf47fd48deafe98ffb8844542def
Thunraz <julian.dinges@gmail.com>
2016-07-11 11:15:49 +0200

    Using own camera class as a wrapper

---
+++ js/app/classes/Camera.js
@@ -0,0 +1,35 @@
+define(
+    ['three'],
+    function(THREE) {
+        return class Camera {
+            
+            // ##############################################
+            // # Constructor ################################
+            // ##############################################
+
+            constructor(scene, fov, aspectRatio, near, far, cameraPosition, targetPosition) {
+                this.scene = scene;
+
+                this.fov            = fov;
+                this.aspectRatio    = aspectRatio;
+                this.near           = near;
+                this.far            = far;
+                this.cameraPosition = cameraPosition;
+                this.targetPosition = targetPosition;
+
+                this.pivot = new THREE.Object3D();
+                this.pivot.position.set(targetPosition.x, targetPosition.y, targetPosition.z);
+
+                this.camera = new THREE.PerspectiveCamera(fov, aspectRatio, near, far);
+                this.camera.position.set(cameraPosition.x, cameraPosition.y, cameraPosition.z);
+                this.camera.zoom = 5;
+                this.camera.lookAt(this.pivot.position);
+                this.camera.updateProjectionMatrix();
+
+                this.pivot.add(this.camera);
+                scene.add(this.pivot);
+                scene.camera = this;
+            }
+        }
+    }
+);
