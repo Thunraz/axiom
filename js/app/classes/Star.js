2418d5b7bd86fe75c13a91502449b7199ccbc0d6
Thunraz <julian.dinges@gmail.com>
2016-07-12 20:26:48 +0200

    Some more fun with PerlinNoise

--- js/app/classes/Star.js
+++ js/app/classes/Star.js
@@ -53,8 +53,8 @@ define(
 
                 let geometry = new THREE.SphereGeometry(this.radius / 100, 32, 32);
                 let material = new THREE.MeshPhongMaterial({
-                    emissive: this.color,
-                    emissiveMap: perlinNoise.NoiseTexture(512, 512),
+                    emissive: this.color & 0x333333,
+                    emissiveMap: perlinNoise.NoiseTexture(32, 32),
                     //map: perlinNoise.NoiseTexture(256, 256)
                 });
                 
