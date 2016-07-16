88bd932325cd10909ed4bd50f55da1a77ca6a67e
Thunraz <julian.dinges@gmail.com>
2016-07-16 18:26:22 +0200

    Using OrbitControls to move/rotate the camera

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2,14 +2,27 @@
 (function() {
     requirejs.config({
         paths: {
-            'three': 'lib/three',
-            'stats': 'lib/stats.min'
+            'three'            : 'lib/three',
+            'OrbitControls'    : 'lib/OrbitControls',
+            'TrackballControls': 'lib/TrackballControls',
+            'stats'            : 'lib/stats.min'
         },
 
         shim: {
             'three': {
                 exports: 'THREE'
             },
+            
+            'OrbitControls': {
+                deps: ['three'],
+                exports: 'OrbitControls'
+            },
+            
+            'TrackballControls': {
+                deps: ['three'],
+                exports: 'TrackballControls'
+            },
+
             'stats': {
                 exports: 'Stats'
             }
