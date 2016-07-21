57ca812c3820cffe7dc481e2e859b528511fff29
Thunraz <julian.dinges@gmail.com>
2016-07-21 21:22:39 +0200

    Player's position indicator is an arrow

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -20,30 +20,32 @@ define(
                 options.model = 'assets/models/ship.json';
 
                 super(scene, name, options);
+                this.positionIndicatorScale = 1;
 
                 this.accelerationChange = new THREE.Vector3(0, 0, 0);
-
-                this._createOrientationArrow();
             }
 
             // ##############################################
             // # Private functions ##########################
             // ##############################################
 
-            _createOrientationArrow() {
+            _createPositionIndicator() {
                 let arrowGeometry = new THREE.Geometry();
 
+                let xOffset = 3;
+                let zOffset = 3;
+
                 // Push vertices for the arrow
-                arrowGeometry.vertices.push(new THREE.Vector3(0, 0, 6));
-                arrowGeometry.vertices.push(new THREE.Vector3(3, 0, 0));
-                arrowGeometry.vertices.push(new THREE.Vector3(6, 0, 6));
-                arrowGeometry.vertices.push(new THREE.Vector3(3, 0, 4));
-                arrowGeometry.vertices.push(new THREE.Vector3(0, 0, 6));
+                arrowGeometry.vertices.push(new THREE.Vector3(0 - xOffset, 0, 6 - zOffset));
+                arrowGeometry.vertices.push(new THREE.Vector3(3 - xOffset, 0, 0 - zOffset));
+                arrowGeometry.vertices.push(new THREE.Vector3(6 - xOffset, 0, 6 - zOffset));
+                arrowGeometry.vertices.push(new THREE.Vector3(3 - xOffset, 0, 4 - zOffset));
+                arrowGeometry.vertices.push(new THREE.Vector3(0 - xOffset, 0, 6 - zOffset));
                 
-                let arrowMaterial = new THREE.LineBasicMaterial({ color: 0x7fffd4 });
-                this.arrow        = new THREE.Line(arrowGeometry, arrowMaterial);
-                this.arrow.position.set(this.position.x, this.position.y, this.position.z);
-                this.scene.add(this.arrow);
+                let arrowMaterial      = new THREE.LineBasicMaterial({ color: 0x7fffd4 });
+                this.positionIndicator = new THREE.Line(arrowGeometry, arrowMaterial);
+                this.positionIndicator.position.set(this.position.x, this.position.y, this.position.z);
+                this.scene.add(this.positionIndicator);
             }
             
             // ##############################################
@@ -64,8 +66,8 @@ define(
 
             turnLeft() {
                 // Get the angle in radians so we can add to it
-                let angle = Math.atan2(this.orientation.y, this.orientation.x);                
-                angle += 180 / Math.PI / 360 / 30;
+                let angle = Math.atan2(this.orientation.y, this.orientation.x);
+                angle += Math.PI / 60;
 
                 this.orientation.x = Math.cos(angle);
                 this.orientation.y = Math.sin(angle);
@@ -75,8 +77,8 @@ define(
 
             turnRight() {
                 // Get the angle in radians so we can add to it
-                let angle = Math.atan2(this.orientation.y, this.orientation.x);                
-                angle -= 180 / Math.PI / 360 / 30;
+                let angle = Math.atan2(this.orientation.y, this.orientation.x);
+                angle -= Math.PI / 60;
 
                 this.orientation.x = Math.cos(angle);
                 this.orientation.y = Math.sin(angle);
@@ -87,15 +89,9 @@ define(
             update(deltaT, smoothDeltaT, spaceObjects) {
                 super.update(deltaT, smoothDeltaT, spaceObjects);
 
-                this.arrow.position.set(this.position.x, this.position.y, this.position.z);
-
-                Debug.log(this.orientation);
-
-                Debug.log(
-                    Math.round(this.acceleration.x * 10000) / 10000 + ' ' +
-                    Math.round(this.acceleration.y * 10000) / 10000 + ' ' +
-                    Math.round(this.acceleration.z * 10000) / 10000
-                );
+                if(this.positionIndicator) {
+                    this.positionIndicator.rotation.y = Math.atan2(this.orientation.y, this.orientation.x) - Math.PI / 2;
+                }
             }
 
             // ##############################################
