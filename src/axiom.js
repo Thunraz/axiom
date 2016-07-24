f589a9282e770e0c2acdb51072ae771aced785c4
Thunraz <julian.dinges@gmail.com>
2016-07-24 19:09:21 +0200

    Fog starts further away now

--- src/axiom.js
+++ src/axiom.js
@@ -26,7 +26,7 @@ define(
         config.maxAnisotropy = renderer.getMaxAnisotropy();
 
         let scene = new THREE.Scene();
-        scene.fog = new THREE.Fog(0x000000, 100, 300);
+        scene.fog = new THREE.Fog(0x000000, 100, 1000);
 
         // Create camera
         let camera = new Camera(
