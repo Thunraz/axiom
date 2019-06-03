9972f8309ea7726b1f043a7310638554db35a315
Thunraz <thunraz@thunraz.com>
2019-06-03 10:26:03 +0200

    Exclude lib directory from linting

--- tasks/compileScript.js
+++ tasks/compileScript.js
@@ -39,7 +39,7 @@ function lint() {
     // So, it's best to have gulp ignore the directory as well.
     // Also, Be sure to return the stream from the task;
     // Otherwise, the task may end before the stream has finished.
-    return gulp.src(['src/js/**/*.js'])
+    return gulp.src(['src/js/**/*.js', '!src/js/lib/**/*.*'])
         // eslint() attaches the lint output to the "eslint" property
         // of the file object so it can be used by other modules.
         .pipe(eslint())
